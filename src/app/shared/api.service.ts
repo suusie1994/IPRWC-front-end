454a73892c53344d94fc69df4c335f8bf98119b9
Suzanne Blom <s1075284@student.hsleiden.nl>
2021-10-31 23:30:12 +0100

    removed code in comments

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -8,33 +8,6 @@ import { AuthService } from '../auth/auth.service';
 export class ApiService {
     constructor(private authService: AuthService) {}
 
-    // private createQueryString(queryParameters: Object): string
-    // {
-    //   let queryString = '';
-
-    //   if (typeof queryParameters === 'object')
-    //   {
-    //     // tslint:disable-next-line: forin
-    //     for (const key in queryParameters)
-    //     {
-    //       console.log(queryParameters, typeof queryParameters);
-    //       const value = queryParameters[key];
-    //       const prefix = queryString.length === 0 ? '?' : '&';
-
-    //       queryString += `${prefix}${key}=${value}`;
-    //     }
-    //   }
-
-    //     return queryString;
-    // }
-
-    // private createURI(path: string, queryParameters: Object): string
-    // {
-    //     const queryString = this.createQueryString(queryParameters);
-
-    //     return `http://localhost:8080/api/${path}${queryString}`;
-    // }
-
     public createRequestHeaders(): HttpHeaders
     {
       let headers = new HttpHeaders();
@@ -48,48 +21,4 @@ export class ApiService {
       }
       return headers;
     }
-
-    // public get<T>(path: string, queryParameters?: Object): Observable<T>|any
-    // {
-    //   let uri = '';
-    //   if (typeof queryParameters === 'object') {
-    //     uri = this.createURI(path, queryParameters);
-    //   }
-    //   const headers = this.createRequestHeaders();
-
-    //   return this.http.get<T>(uri, { headers });
-    // }
-
-    // public post<T>(path: string, data: Object, queryParameters?: Object): Observable<T>
-    // {
-    //   let uri = '';
-    //   if (typeof queryParameters === 'object') {
-    //     uri = this.createURI(path, queryParameters);
-    //   }
-    //   const headers = this.createRequestHeaders();
-
-    //   return this.http.post<T>(uri, data, { headers });
-    // }
-
-    // public put<T>(path: string, data: any, queryParameters?: Object): Observable<T>
-    // {
-    //   let uri = '';
-    //   if (typeof queryParameters === 'object') {
-    //     uri = this.createURI(path, queryParameters);
-    //   }
-    //   const headers = this.createRequestHeaders();
-
-    //   return this.http.put<T>(uri, data, { headers });
-    // }
-
-    // public delete<T>(path: string, queryParameters?: Object): Observable<T>
-    // {
-    //   let uri = '';
-    //   if (typeof queryParameters === 'object') {
-    //     uri = this.createURI(path, queryParameters);
-    //   }
-    //   const headers = this.createRequestHeaders();
-
-    //   return this.http.delete<T>(uri, { headers });
-    // }
 }
