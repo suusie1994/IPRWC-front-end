c692f5de03043de45d8b1ca8cec4feb18c36f087
Suzanne Blom <s1075284@student.hsleiden.nl>
2022-03-30 11:44:22 +0200

    removed comments

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -9,21 +9,6 @@ import { AuthService } from './auth.service';
 })
 export class AuthGuard implements CanActivate {
 
-  // constructor(private router: Router,
-  //             private authservice: AuthService,
-  //             private userService: UserService) {}
-
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-  //     if (this.userService.isUserLoggedIn()) {
-  //       return true;
-  //     }
-
-  //     this.router.navigate(['/products']);
-  //     return false;
-  // }
-
   constructor(private router: Router, private userService: UserService) {}
 
     canActivate(
